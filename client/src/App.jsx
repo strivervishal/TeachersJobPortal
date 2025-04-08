d709b5061100fb9457b60f7e5d467a0c09380fb0
strivervishal <strivervishal@gmail.com>
2025-04-08 12:31:31 +0530

    frontend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+// App.jsx
+import React from "react";
+
+function App() {
+  return (
+    <div className="text-center text-3xl p-4">
+      <h1>Welcome to Teachers Job Portal</h1>
+    </div>
+  );
+}
+
+export default App;
