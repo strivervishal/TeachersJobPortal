0ce4fdea56261ab03c6d66663361dc036f805e2f
Ankush Jagadale <jagadaleankush10@gmail.com>
2025-04-14 22:23:05 +0530

    Added Componets Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 // App.jsx
 import React from "react";
+import ContactPage from "./components/ContactPage";
 
 function App() {
   return (
     <div className="text-center text-3xl p-4">
-      <h1>Welcome to Teachers Job Portal</h1>
+      {/* <h1>Welcome to Teachers Job Portal</h1> */}
+      <ContactPage/>
     </div>
   );
 }
