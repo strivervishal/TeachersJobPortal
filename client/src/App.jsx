98e3c627e3cb5258800fcc9a678dfe39d3c6a6eb
ajay-patil2003 <ap6356617@gmail.com>
2025-04-15 18:43:57 +0530

    footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
 // App.jsx
 import React from "react";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="text-center text-3xl p-4">
-      <h1>Welcome to Teachers Job Portal</h1>
+      
+      <>
+      
+      <Footer />
+    </>
+      
     </div>
   );
 }
